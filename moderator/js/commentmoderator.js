f8aebcd8fd92d9a5053270613bc406006b6e33fa
Charles Barrows <charles.barrows@pearson.com>
2015-12-16 10:26:50 -0500

    comment sorting

--- moderator/js/commentmoderator.js
+++ moderator/js/commentmoderator.js
@@ -1,5 +1,19 @@
 define(["jquery.ui", "firebase", "bootstrap-confirmation"], function () {
 
+	// sort by title, unmoderated, timestamp
+	function sortComments (a, b) {
+		if (a.title < b.title)
+			return -1;
+		else if (a.title > b.title)
+			return 1;
+		else {
+			if (a.ok === false) return -1;
+			else if (b.ok === false) return 1;
+			else
+				return (a.timestamp - b.timestamp);
+		}
+	}
+
 	$.widget("que.CommentModerator", {
 
 		options: {},
@@ -66,60 +80,70 @@ define(["jquery.ui", "firebase", "bootstrap-confirmation"], function () {
 
 			var lastTitle;
 
-			for (var each in data) {
-				var titleName = each;
-				var title = data[each];
+			for (var i = 0; i < data.length; i++) {
+				var rec = data[i];
+				var title = rec.title;
 
-				if (titleName != lastTitle) {
+				if (title != lastTitle) {
 					var row = $("<tr>");
 					el.append(row);
 
-					var td = $("<td>", {text: titleName});
+					var td = $("<td>", {text: title});
 					td.attr("colspan", 9);
 					row.append(td);
 					row.addClass("success");
 
-					lastTitle = titleName;
+					lastTitle = title;
 				}
 
-				for (var c in title.comments) {
-					var rec = title.comments[c];
+				var row = $("<tr>");
+				var key = title + "/comments/" + rec.key;
+				row.attr("data-key", key);
+				el.append(row);
 
-					var row = $("<tr>");
-					var key = titleName + "/comments/" + c;
-					row.attr("data-key", key);
-					el.append(row);
+				var dateFromTimestamp = new Date(rec.timestamp);
+				var dateOptions = { year: "numeric", month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit" };
+				var dateFormatted = dateFromTimestamp.toLocaleTimeString([], dateOptions);
 
-					var dateFromTimestamp = new Date(rec.timestamp);
-					var dateOptions = { year: "numeric", month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit" };
-					var dateFormatted = dateFromTimestamp.toLocaleTimeString([], dateOptions);
+				var checkbox = $("<input>", { type: "checkbox" });
+				checkbox.click($.proxy(this.onClickOK, this));
+				if (rec.ok)
+					checkbox.prop("checked", "true");
 
-					var checkbox = $("<input>", { type: "checkbox" });
-					checkbox.click($.proxy(this.onClickOK, this));
-					if (rec.ok)
-						checkbox.prop("checked", "true");
+				var deleteButton = $("<button>", { class: "btn btn-danger"}).append($("<span>", { class: "glyphicon glyphicon-remove"}));
+				deleteButton.attr( { "data-toggle": "confirmation", "data-placement": "left" } );
 
-					var deleteButton = $("<button>", { class: "btn btn-danger"}).append($("<span>", { class: "glyphicon glyphicon-remove"}));
-					deleteButton.attr( { "data-toggle": "confirmation", "data-placement": "left" } );
-
-					row.append($("<td>", {text: titleName}))
-						.append($("<td>", {text: dateFormatted}))
-						.append($("<td>", {text: rec.category}))
-						.append($("<td>", {text: rec.parent ? "yes" : "no"}))
-						.append($("<td>", {text: rec.name}))
-						.append($("<td>", {text: rec.email}))
-						.append($("<td>", {text: rec.text}))
-						.append($("<td>", { class: "text-center ok-row" }).append(checkbox))
-						.append($("<td>", { class: "text-center" }).append(deleteButton));
-				}
+				row.append($("<td>", {text: title}))
+					.append($("<td>", {text: dateFormatted}))
+					.append($("<td>", {text: rec.category}))
+					.append($("<td>", {text: rec.parent ? "yes" : "no"}))
+					.append($("<td>", {text: rec.name}))
+					.append($("<td>", {text: rec.email}))
+					.append($("<td>", {text: rec.text}))
+					.append($("<td>", { class: "text-center ok-row" }).append(checkbox))
+					.append($("<td>", { class: "text-center" }).append(deleteButton));
 			}
 
 			$('[data-toggle="confirmation"]').confirmation( { placement: "left", onConfirm: $.proxy(this.onClickDelete, this) } );
 		},
 
-		// sort by title, unmoderated, timestamp
 		sortData: function (data) {
-			return data;
+			var recs = [];
+
+			for (var title in data) {
+				var t = data[title];
+
+				for (var c in t.comments) {
+					var comment = t.comments[c];
+					var rec = { title: title, key: c };
+					$.extend(rec, comment);
+					recs.push(rec);
+				}
+			}
+
+			recs = recs.sort(sortComments);
+
+			return recs;
 		},
 
 		onClickOK: function (event) {
