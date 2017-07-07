5fbd747b29ecf5ea29b0d346e2961fb2f1923839
Charles Barrows <charles.barrows@pearson.com>
2017-07-07 12:31:51 -0400

    compressing progress data

--- completecourse/js/database.js
+++ completecourse/js/database.js
@@ -14,10 +14,20 @@ define(["common"], function (Common) {
 		}
 		var st = s.join("");
 
+		// lzw-compress it and prepend an *
+		st = "*" + lzw_encode(st);
+
 		return st;
 	}
 
 	function unshrink (st) {
+		// * = lzw-compressed
+		if (st[0] === "*") {
+			console.log("items compressed " + st.length);
+			st = lzw_decode(st.substr(1));
+			console.log("items uncompressed " + st.length);
+		}
+
 		var itemsArray = [];
 		var n = st.length / 2;
 		for (var i = 0; i < n; i++) {
@@ -28,6 +38,60 @@ define(["common"], function (Common) {
 			itemsArray[i] = obj;
 		}
 		return itemsArray;
+	}
+
+	// FROM: https://gist.github.com/revolunet/843889
+	// LZW-compress a string
+	function lzw_encode (s) {
+		var dict = {};
+		var data = (s + "").split("");
+		var out = [];
+		var currChar;
+		var phrase = data[0];
+		var code = 256;
+		for (var i=1; i<data.length; i++) {
+			currChar=data[i];
+			if (dict['_' + phrase + currChar] != null) {
+				phrase += currChar;
+			}
+			else {
+				out.push(phrase.length > 1 ? dict['_'+phrase] : phrase.charCodeAt(0));
+				dict['_' + phrase + currChar] = code;
+				code++;
+				phrase=currChar;
+			}
+		}
+		out.push(phrase.length > 1 ? dict['_'+phrase] : phrase.charCodeAt(0));
+		for (var i=0; i<out.length; i++) {
+			out[i] = String.fromCharCode(out[i]);
+		}
+		return out.join("");
+	}
+
+	// Decompress an LZW-encoded string
+	function lzw_decode (s) {
+		var dict = {};
+		var data = (s + "").split("");
+		var currChar = data[0];
+		var oldPhrase = currChar;
+		var out = [currChar];
+		var code = 256;
+		var phrase;
+		for (var i=1; i<data.length; i++) {
+			var currCode = data[i].charCodeAt(0);
+			if (currCode < 256) {
+				phrase = data[i];
+			}
+			else {
+				phrase = dict['_'+currCode] ? dict['_'+currCode] : (oldPhrase + currChar);
+			}
+			out.push(phrase);
+			currChar = phrase.charAt(0);
+			dict['_'+code] = oldPhrase + currChar;
+			code++;
+			oldPhrase = phrase;
+		}
+		return out.join("");
 	}
 
 	function GetCommentsForTitle (isbn, callback) {
